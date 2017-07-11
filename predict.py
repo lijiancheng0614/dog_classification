9cf9cc13c8b126c77674c925962a3c99cdcd0146
Jiancheng Li <lijiancheng@sensetime.com>
2017-07-11 20:24:18 +0800

    UPDATE data_transform for predict.py

--- predict.py
+++ predict.py
@@ -19,7 +19,7 @@ caffe.set_mode_gpu()
 def test(image_path, net):
     transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
     transformer.set_transpose('data', (2,0,1))
-    transformer.set_raw_scale('data', 255)
+    transformer.set_mean('data', np.array([104, 117, 123]))
     transformer.set_channel_swap('data', (2,1,0))
     image = caffe.io.load_image(image_path)
     net.blobs['data'].data[...] = transformer.preprocess('data', image)
